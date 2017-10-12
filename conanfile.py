871a3d0052e7de79c549298dc9a50646a58a1e2d
Luis Díaz Más <piponazo@gmail.com>
2017-10-12 19:46:06 +0200

    Require OpenCV with ffmpeg and webcam support

--- conanfile.py
+++ conanfile.py
@@ -4,5 +4,10 @@ class WyllisDetectorConan(ConanFile):
     settings = 'os', 'compiler', 'build_type', 'arch'
     generators = 'cmake'
 
+    def configure(self):
+        self.options['OpenCV'].shared = True
+        self.options['OpenCV'].ffmpeg = True
+        self.options['OpenCV'].webcam = True
+
     def requirements(self):
         self.requires('OpenCV/3.3.0@piponazo/testing')
