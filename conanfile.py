4b22a97aad36cbae6f4fa627ff0772419196322f
Luis Diaz Mas <piponazo@gmail.com>
2017-11-29 19:40:32 +0100

    Require OpenCV with camera option

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,7 @@ from conans import ConanFile
 class WyllisDetectorConan(ConanFile):
     settings = 'os', 'compiler', 'build_type', 'arch'
     generators = 'cmake'
+    default_options = "OpenCV:webcam=True"
 
     def requirements(self):
         self.requires('OpenCV/3.3.0@piponazo/testing')
