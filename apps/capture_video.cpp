67294fb827ed87fd5e108e47731b0295fb5c0614
Luis Díaz Más <piponazo@gmail.com>
2017-12-05 21:48:06 +0100

    Add more options in the capture app to set webcam properties

--- apps/capture_video.cpp
+++ apps/capture_video.cpp
@@ -19,9 +19,16 @@ using namespace std;
 
 const cv::String keys {
     "{help h         |          | print this message   }"
-    "{index          |0         | Camera index (default = 0)}"
-    "{seconds        |2         | Time during which the app will capture frames }"
+    "{index          |0         | Camera index }"
+    "{seconds s      |2         | Time during which the app will capture frames }"
     "{output         |live.avi  | Time during which the app will capture frames }"
+    "{brightness br  |160.      | Brightness value }"
+    "{contrast cr    |42.       | Constrast value }"
+    "{exposure ex    |-4.       | Exposure value }"
+    "{focus f        |200       | Focus value }"
+    "{width          |1280      | Width size in pixels}"
+    "{height         |720       | Height size in pixels}"
+    "{fps            |15        | FPS for the output video}"
     "{ts timestamp   |          | use time stamp       }"
 };
 
@@ -49,11 +56,33 @@ int main(int argc, char **argv)
     }
 
     cout << "Camera connected correctly" << endl;
+    cout << "Initial Frame width: " << capture.get(cv::CAP_PROP_FRAME_WIDTH) << endl;
+    cout << "Initial Frame height: " << capture.get(cv::CAP_PROP_FRAME_HEIGHT) << endl;
+
+    capture.set(cv::CAP_PROP_BRIGHTNESS, cmdParser.get<double>("brightness"));
+    capture.set(cv::CAP_PROP_CONTRAST, cmdParser.get<double>("contrast"));
+    capture.set(cv::CAP_PROP_EXPOSURE, cmdParser.get<double>("exposure"));
+    capture.set(cv::CAP_PROP_FOCUS, cmdParser.get<double>("focus"));
+    capture.set(cv::CAP_PROP_FRAME_WIDTH, cmdParser.get<double>("width"));
+    capture.set(cv::CAP_PROP_FRAME_HEIGHT, cmdParser.get<double>("height"));
+
+    cout << "BRIGHTNESS: " << capture.get(cv::CAP_PROP_BRIGHTNESS) << endl;
+    cout << "Constrast: " << capture.get(cv::CAP_PROP_CONTRAST) << endl;
+    cout << "Saturation: " << capture.get(cv::CAP_PROP_SATURATION) << endl;
+    cout << "Exposure: " << capture.get(cv::CAP_PROP_EXPOSURE) << endl;
+    cout << "Focus: " << capture.get(cv::CAP_PROP_FOCUS) << endl;
+    cout << "Frame width: " << capture.get(cv::CAP_PROP_FRAME_WIDTH) << endl;
+    cout << "Frame height: " << capture.get(cv::CAP_PROP_FRAME_HEIGHT) << endl;
 
     cv::Mat frame;
 
     // get one frame from camera to know frame size and type
     capture >> frame;
+
+    if (capture.get(cv::CAP_PROP_FOCUS) == 0) {
+        cout << "Focus is 0: It seems that the auto-focus is enabled. Please disable it" << endl;
+        return EXIT_FAILURE;
+    }
 
     // check if we succeeded
     if (frame.empty()) {
@@ -66,8 +95,7 @@ int main(int argc, char **argv)
     //--- INITIALIZE VIDEOWRITER
     cv::VideoWriter writer;
     int codec = CV_FOURCC('M', 'J', 'P', 'G');  // select desired codec (must be available at runtime)
-    double fps = 25.0;                          // framerate of the created video stream
-    writer.open(outputPath, codec, fps, frame.size(), isColor);
+    writer.open(outputPath, codec, cmdParser.get<double>("fps"), frame.size(), isColor);
 
     if (!writer.isOpened()) {
         cerr << "Could not open the output video file for write\n";
